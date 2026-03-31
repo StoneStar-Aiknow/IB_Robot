07b6fd35923f2fb68f5f44ef59a7a7d9f4c69c73
XiaoqiangWu <wuxiaoqiang.rtos@huawei.com>
2026-03-31 19:42:20 +0800

    fix(sdk): export IssueService in services package
    Add IssueService to the __all__ list in atomgit_sdk.services to ensure
    proper high-level API access for issue management.
    
    Signed-off-by: XiaoqiangWu <wuxiaoqiang.rtos@huawei.com>

--- libs/atomgit_sdk/src/atomgit_sdk/services/__init__.py
+++ libs/atomgit_sdk/src/atomgit_sdk/services/__init__.py
@@ -4,5 +4,6 @@ Services for AtomGit SDK
 
 from atomgit_sdk.services.pr_service import PRService
 from atomgit_sdk.services.repair_service import RepairService
+from atomgit_sdk.services.issue_service import IssueService
 
-__all__ = ["PRService", "RepairService"]
+__all__ = ["PRService", "RepairService", "IssueService"]
